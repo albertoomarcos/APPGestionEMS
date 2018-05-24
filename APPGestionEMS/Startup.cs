cf87ef2ad2239182f88c8c6657928a987e400510
albertoomarcos <albertoo.marcos@gmail.com>
2018-05-24 15:52:20 +0200

    Initial commit

---
+++ APPGestionEMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APPGestionEMS.Startup))]
+namespace APPGestionEMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
